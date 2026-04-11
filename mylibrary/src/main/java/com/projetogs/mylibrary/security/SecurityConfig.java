6e2b942e04600dc022914f400d386a14aa520010
GabrielAdosS <gabriel.17.set.2005@gmail.com>
2026-04-10 22:32:45 -0300

    feat(security): Criando configuração básica para o spring security

---
+++ mylibrary/src/main/java/com/projetogs/mylibrary/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.projetogs.mylibrary.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .requestMatchers("/users/**").permitAll()
+                        .anyRequest().authenticated())
+                .build();
+    }
+}

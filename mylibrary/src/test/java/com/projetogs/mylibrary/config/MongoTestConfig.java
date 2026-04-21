6879ec72e6ad5b1df15d8da7bea63a8fc762c624
Erick Badaró <zaterick10@gmail.com>
2026-04-20 23:17:42 -0300

    feat(tests): adicionando configuração de testes com MongoDB usando Testcontainers

---
+++ mylibrary/src/test/java/com/projetogs/mylibrary/config/MongoTestConfig.java
@@ -0,0 +1,21 @@
+package com.projetogs.mylibrary.config;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+
+public abstract class MongoTestConfig {
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+}

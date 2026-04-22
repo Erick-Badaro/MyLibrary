d62a450239dcbd380687db1e42beb95ff26e747a
Erick Badaró <zaterick10@gmail.com>
2026-04-22 20:49:20 -0300

    feat(tests): configurar testes com MongoDB e adicionar propriedades de teste

--- mylibrary/src/test/java/com/projetogs/mylibrary/config/MongoTestConfig.java
+++ mylibrary/src/test/java/com/projetogs/mylibrary/config/MongoTestConfig.java
@@ -1,6 +1,7 @@
 package com.projetogs.mylibrary.config;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
@@ -8,14 +9,19 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@Testcontainers
+@ActiveProfiles("test")
 
 public abstract class MongoTestConfig {
-    @Container
-    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0");
+
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4");
+
+    static {
+        mongoDBContainer.start();
+    }
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry){
         registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+        registry.add("spring.data.mongodb.database", () -> "mylibrary_test");
     }
 }

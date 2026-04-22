890d37677e6dd4730303c0bf8a312418ae9d0cc1
Erick Badaró <zaterick10@gmail.com>
2026-04-22 01:33:25 -0300

    feat(tests): estender MylibraryApplicationTests com configuração de teste do MongoDB

--- mylibrary/src/test/java/com/projetogs/mylibrary/MylibraryApplicationTests.java
+++ mylibrary/src/test/java/com/projetogs/mylibrary/MylibraryApplicationTests.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.projetogs.mylibrary.config.MongoTestConfig;
+
 @SpringBootTest
 @ActiveProfiles("test")
-class MylibraryApplicationTests {
+class MylibraryApplicationTests extends MongoTestConfig {
 
 	@Test
 	void contextLoads() {

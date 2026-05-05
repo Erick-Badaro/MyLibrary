ffb60d609fdfdbad5be5586b0ddca65c163932b6
gabriel .a dos santos <gabriel.17.set.2005@gmail.com>
2026-05-05 12:17:47 -0300

    fix(testContainer): removendo uri fixa do banco de dados para os test

--- mylibrary/src/test/java/com/projetogs/mylibrary/config/MongoTestConfig.java
+++ mylibrary/src/test/java/com/projetogs/mylibrary/config/MongoTestConfig.java
@@ -1,9 +1,12 @@
 package com.projetogs.mylibrary.config;
 
+import java.time.Duration;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -11,7 +14,9 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 public abstract class MongoTestConfig {
     @Container
-    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.0");
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.0")
+    .withStartupTimeout(Duration.ofMinutes(2)) // Dá tempo para o Runner baixar a imagem e subir
+        .waitingFor(Wait.forLogMessage(".*Waiting for connections.*\\n", 1)); // Só libera quando o Mongo estiver pronto
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry){

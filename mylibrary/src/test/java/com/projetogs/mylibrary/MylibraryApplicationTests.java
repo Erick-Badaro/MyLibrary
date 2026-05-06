6bb52d8caad2b9c1ebdff3c237208fc604695742
gabriel .a dos santos <gabriel.17.set.2005@gmail.com>
2026-05-06 01:45:58 -0300

    fix: voltando para versão com a configuração na classe

--- mylibrary/src/test/java/com/projetogs/mylibrary/MylibraryApplicationTests.java
+++ mylibrary/src/test/java/com/projetogs/mylibrary/MylibraryApplicationTests.java
@@ -1,10 +1,30 @@
 package com.projetogs.mylibrary;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import com.projetogs.mylibrary.config.MongoTestContainerConfig;
+@SpringBootTest(properties = {
+		"api.security.token.secret=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30",
+})
+@ActiveProfiles("test")
+@Testcontainers
+class MylibraryApplicationTests {
 
-class MylibraryApplicationTests extends MongoTestContainerConfig {
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7");
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+
+		registry.add("spring.data.mongodb.database", () -> "mylibrarytest");
+	}
 
 	@Test
 	void contextLoads() {

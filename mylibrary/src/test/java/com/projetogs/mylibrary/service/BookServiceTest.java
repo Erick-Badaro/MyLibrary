e9c29037a6ba0cd054ab16e8e34c561edc76a503
gabriel .a dos santos <gabriel.17.set.2005@gmail.com>
2026-05-06 12:02:29 -0300

    feat(BookServiceTest): Criando teste de buscar livros pelo id do usuário e também pelo status, e criando teste de cadastro do livro

--- mylibrary/src/test/java/com/projetogs/mylibrary/service/BookServiceTest.java
+++ mylibrary/src/test/java/com/projetogs/mylibrary/service/BookServiceTest.java
@@ -1,31 +1,111 @@
 package com.projetogs.mylibrary.service;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
 import org.bson.types.ObjectId;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import com.projetogs.mylibrary.config.MongoTestContainer;
 import com.projetogs.mylibrary.dto.BookDTO;
 import com.projetogs.mylibrary.enums.ReadingStatus;
+import com.projetogs.mylibrary.repository.BookRepository;
 
-public class BookServiceTest  extends MongoTestContainer{
+@SpringBootTest(properties = {
+        "api.security.token.secret=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30"
+})
+@Testcontainers
+@ActiveProfiles("test")
+public class BookServiceTest {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+
+        registry.add("spring.data.mongodb.database", () -> "mylibrarytest");
+    }
 
     @Autowired
     private BookService service;
 
-    private String userIdTest = new ObjectId().toHexString();
+    @Autowired
+    private BookRepository repository;
+
+    private String userIdTest;
+
+    @BeforeEach
+    public void setUp() {
+        userIdTest = new ObjectId().toHexString();
+        BookDTO livroParaLer = new BookDTO(
+                "O Silmarillion",
+                "J.R.R. Tolkien",
+                "HarperCollins",
+                "Fantasia",
+                ReadingStatus.WANNA_READ);
+
+        BookDTO livroLendo = new BookDTO(
+                "1984",
+                "George Orwell",
+                "Companhia das Letras",
+                "Distopia",
+                ReadingStatus.READING);
+
+        BookDTO livroLido = new BookDTO(
+                "O Hobbit",
+                "J.R.R. Tolkien",
+                "HarperCollins",
+                "Fantasia",
+                ReadingStatus.READ);
+
+        service.createBook(userIdTest, livroParaLer);
+        service.createBook(userIdTest, livroLendo);
+        service.createBook(userIdTest, livroLido);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        repository.deleteAll();
+    }
 
     @Test
     @DisplayName("Deve buscar os livros de um usuário no banco")
-    public void getBooksByUserId() {
-        BookDTO novoLivro = new BookDTO("O Hobbit", "J.R.R. Tolkien", "Fantasia", "HarperCollins", ReadingStatus.READ);
-        service.createBook(userIdTest, novoLivro);
+    public void testGetBooksByUserId() {
         List<BookDTO> result = service.getBooksByUserId(userIdTest);
         assertTrue(!result.isEmpty());
     }
+
+    @Test
+    @DisplayName("Deve buscar os livros de um usuário pelo id e pelo status de leitura no banco")
+    public void testGetBooksByUserIdAndStatus() {
+        List<BookDTO> resultRead = service.getBooksByUserIdAndStatus(userIdTest, ReadingStatus.READ);
+        List<BookDTO> resultReading = service.getBooksByUserIdAndStatus(userIdTest, ReadingStatus.READING);
+        List<BookDTO> resultWannaRead = service.getBooksByUserIdAndStatus(userIdTest, ReadingStatus.WANNA_READ);
+        assertTrue(!resultRead.isEmpty());
+        assertTrue(!resultReading.isEmpty());
+        assertTrue(!resultWannaRead.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Deve criar um livro relacionado a um usuário")
+    public void testCreateBook() {
+        BookDTO book = new BookDTO("Código limpo", "Robert C. Martin", "AltaBooks", "TI", ReadingStatus.READING);
+        BookDTO saved = service.createBook(userIdTest, book);
+        assertNotNull(saved);
+    }
+
 }
